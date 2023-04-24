a25e08bfe2ca456c3f18c4bb94981a19d630df71
unknown <marcuspacc@gmail.com>
2023-04-23 17:05:02 -0700

    set up starter code and set up App.js file

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import AboutMe from './pages/AboutMe';
+import NoMatch from './pages/NoMatch';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={<AboutMe />}
+          />
+          <Route
+            path="/aboutMe"
+            element={<Portfolio />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+          <Route
+            path="/resume"
+            element={<Resume />}
+          />
+          <Route
+            path="*"
+            element={<NoMatch />}
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
